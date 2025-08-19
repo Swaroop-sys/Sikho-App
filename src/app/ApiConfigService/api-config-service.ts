915090661cae02f36c1444993e215b4ee6840478
Swaroop <swaroopshinde02@gmail.com>
2025-08-19 10:18:24 +0530

    apiConfig

---
+++ src/app/ApiConfigService/api-config-service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiConfigService {
+   baseUrl = "https://majestic-rolypoly-d12d8d.netlify.app";
+}
--- src/app/Auth/registration/auth-service.ts
+++ src/app/Auth/registration/auth-service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ApiConfigService } from '../../ApiConfigService/api-config-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private apiConfig:ApiConfigService) {}
  registerUser(userData: any) {
-    return this.http.post('http://localhost:8080/api/auth/register', userData);
+    return this.http.post(`${this.apiConfig}/api/auth/register`, userData);
   }
   loginUser(userData: any):Observable<any> {
-    return this.http.post<any>('http://localhost:8080/api/auth/login', userData);
+    return this.http.post<any>(`${this.apiConfig}/api/auth/login`, userData);
   }
 }
