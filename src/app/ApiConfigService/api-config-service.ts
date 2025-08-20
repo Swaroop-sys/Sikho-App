3a6b2487a6f193a5fe862f1c0590bf777b57fea6
Swaroop <swaroopshinde02@gmail.com>
2025-08-20 13:45:19 +0530

    1:45 PM

--- src/app/ApiConfigService/api-config-service.ts
+++ src/app/ApiConfigService/api-config-service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiConfigService {
-   baseUrl = "https://spectacular-rugelach-ef3fb8.netlify.app";
+   baseUrl = "sikho-app.railway.internal";
 }
